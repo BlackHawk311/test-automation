9bdcfcb09c3cad7028761d9c7c09db422821a5d6
BlackHawk <vergil31@hotmail.fr>
2023-12-01 16:21:39 +0100

    Modification test_github_actions

--- src/services/string_service.py
+++ src/services/string_service.py
@@ -2,8 +2,8 @@ def to_uppercase(value: str) -> str:
     if not value:
         return value
 
-    if not type(value) == str:
-        return value
+    if not isinstance(value, str):
+        return "This value is not a string."
 
     return value.upper()
 
@@ -12,8 +12,8 @@ def to_lowercase(value: str) -> str:
     if not value:
         return value
 
-    if not type(value) == str:
-        return value
+    if not isinstance(value, str):
+        return "This value is not a string."
 
     return value.lower()
 
@@ -22,8 +22,8 @@ def capitalize(value: str) -> str:
     if not value:
         return value
 
-    if not type(value) == str:
-        return value
+    if not isinstance(value, str):
+        return "This value is not a string."
 
     return value.capitalize()
 
@@ -32,10 +32,10 @@ def truncate(value: str, n_char: int) -> str:
     if not value:
         return value
 
-    if not type(value) == str:
-        return value
-
     if len(value) < n_char:
         return value
+
+    if not isinstance(value, str):
+        return "This value is not a string."
 
     return value[:n_char] + '...'
--- tests/test_string_service.py
+++ tests/test_string_service.py
@@ -14,4 +14,4 @@ def test_uppercase_with_no_str():
 
 
 def test_uppercase_with_not_str():
-    assert to_uppercase(2) == 2
+    assert to_uppercase(2) == "This value is not a string."
